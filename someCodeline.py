21aa2884f72b1eba8e17e1722f4835c05a39e191
masonghao <30997918+masonghao@users.noreply.github.com>
2018-05-26 10:05:05 +0800

    Create someCodeline.py

---
+++ someCodeline.py
@@ -0,0 +1,22 @@
+'''
+mysqlLib.py ------------------------------------------------------------------start
+'''
+# -*- coding: UTF-8 -*-
+import MySQLdb
+def get_list(sql = 'SELECT m_time,m_title FROM `ms_news`'):
+    # 打开数据库连接
+    db = MySQLdb.connect("localhost", "root", "", "msbooks", charset='utf8' )
+    cursor = db.cursor() # 使用cursor()方法获取操作游标 
+    cursor.execute(sql) # 使用execute方法执行SQL语句
+    data = cursor.fetchall() # 使用 fetchone() 方法获取一条数据
+    # 关闭数据库连接
+    db.close()
+    return data
+
+if __name__ == '__main__':
+    sql = 'SELECT m_time,m_author,m_title FROM `ms_news`'
+    for x in get_list(sql):
+        print(*x)
+'''
+mysqlLib.py ------------------------------------------------------------------ end
+'''
